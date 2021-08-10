16d31172e11bc91d2c9eee46b99d6d4b1bcae2b2
ronaldojudice <ronaldojudice@live.com>
2021-08-10 13:53:07 -0300

    FIRST COMMIT

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins:['prettier'],
+  global:{
+    Atomics:'readonly',
+    SharedArrayBuffer:'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off', // desativa uma regra que diz que todo método de uma classe precisa usar a palavra this
+    'no-param-reassign': 'off', // desativa uma regra que diz que não é permitido receber parâmetro e fazer alterações nele
+    camelcase: 'off', // desativa uma regra que diz que toda variável precisa ser escrita no formato camelCase
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
